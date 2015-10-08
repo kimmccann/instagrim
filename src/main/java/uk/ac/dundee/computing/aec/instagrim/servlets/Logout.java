47ef5cee00a9f2db40013a3e35714eb0f4dfc9f6
Kim <kimberleylmccann@gmail.com>
2015-10-08 23:30:49 +0100

    Implemented Logout feature

---
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Logout.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.dundee.computing.aec.instagrim.servlets;
+import com.datastax.driver.core.Cluster;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import uk.ac.dundee.computing.aec.instagrim.lib.CassandraHosts;
+import uk.ac.dundee.computing.aec.instagrim.models.User;
+import uk.ac.dundee.computing.aec.instagrim.stores.LoggedIn;
+
+/**
+ *
+ * @author Kimberley
+ */
+@WebServlet(name = "Logout", urlPatterns = {"/Logout"})
+public class Logout extends HttpServlet{
+    
+     @Override
+     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         //Gets current session
+         HttpSession session=request.getSession();
+         LoggedIn lg= new LoggedIn();
+         //Sets logedin = false
+         lg.setLogedout();
+         session.setAttribute("LoggedIn", lg);
+         System.out.println("Session in servlet "+session);
+         System.out.println("user logged out");
+         //Forwards to homepage
+         response.sendRedirect("/Instagrim");
+     }
+     
+    
+}
