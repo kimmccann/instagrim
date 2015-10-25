0284e28a841aaebbba0769387f73bf97c0f0f19c
Kim <kimberleylmccann@gmail.com>
2015-10-25 15:37:36 +0000

    Simplified the comments table

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/lib/Keyspaces.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/lib/Keyspaces.java
@@ -51,7 +51,6 @@ public final class Keyspaces {
             String CreateCommentTable = "CREATE TABLE if not exists instagrim.comments (\n"
                     + "     user varchar,\n"
                     + "     picid UUID,\n"
-                    + "     pic_added timestamp,\n"
                     + "     comments varchar,\n"
                     + "     PRIMARY KEY (picid, pic_added)\n"
                     + " ) WITH CLUSTERING ORDER BY (pic_added desc);";
